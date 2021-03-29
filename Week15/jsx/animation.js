5ace6669b9cb2e5d4d06ee5c4be24e2304da2dfd
PsChina <panshanzxcv.love@163.com>
2021-03-29 23:15:34 +0800

    设计时间线的更新

--- Week15/jsx/animation.js
+++ Week15/jsx/animation.js
@@ -3,22 +3,30 @@
 const TICK = Symbol('tick')
 const TICK_HANDLER = Symbol('tick-handler')
 const ANIMATIONS = Symbol('animations')
+const START_TIME = Symbol('start-time')
 export class Timeline {
     constructor() {
 
         this[ANIMATIONS] = new Set()
+        this[START_TIME] = new Map()
     }
     start() {
         let startTime = Date.now()
         this[TICK] = () => {
-            let t = Date.now() - startTime
+            let now = Date.now()
             for (let animation of this[ANIMATIONS]) {
-                let t0 = t
+                let t
+                if (this[START_TIME].get(animation) < startTime) {
+                    t = now - startTime
+                } else {
+                    t = now - this[START_TIME].get(animation)
+                }
+
                 if (animation.duration < t) {
                     this[ANIMATIONS].delete(animation)
-                    t0 = animation.duration
+                    t = animation.duration
                 }
-                animation.receive(t0)
+                animation.receive(t)
             }
             requestAnimationFrame(this[TICK])
         }
@@ -33,8 +41,12 @@ export class Timeline {
     reset() {
 
     }
-    add(animation) {
+    add(animation, startTime) {
+        if (arguments.length < 2) {
+            startTime = Date.now()
+        }
         this[ANIMATIONS].add(animation)
+        this[START_TIME].set(animation, startTime)
     }
     // set rate() {
 
@@ -45,13 +57,14 @@ export class Timeline {
 }
 
 export class Animation {
-    constructor(object, property, startValue, endValue, duration, timingFunction) {
+    constructor(object, property, startValue, endValue, duration, delay, timingFunction) {
         this.object = object
         this.property = property
         this.startValue = startValue
         this.endValue = endValue
         this.duration = duration
         this.timingFunction = timingFunction
+        this.delay = delay
     }
     receive(time) {
         console.log('time=>', time)
