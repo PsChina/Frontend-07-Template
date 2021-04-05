def8a09c8898a4fbadfaaa0b2e0ed53e5981f724
PsChina <panshanzxcv.love@163.com>
2021-04-05 22:41:19 +0800

    完成一个flick

--- Week15/gesture/gesture.js
+++ Week15/gesture/gesture.js
@@ -57,20 +57,16 @@ document.addEventListener('mousedown', event => {
 
 document.addEventListener('touchstart', event => {
     // event 有多个触点
-    console.log(event.changedTouches)
     for (let touch of event.changedTouches) {
         let context = Object.create(null)
-        contexts.set(touch.identifier, contexts)
-        console.log(touch.clientX, touch.clientY)
+        contexts.set(touch.identifier, context)
         start(touch, context)
     }
 })
 
 
 document.addEventListener('touchmove', event => {
-    console.log(event.changedTouches)
     for (let touch of event.changedTouches) {
-        console.log(touch.clientX, touch.clientY)
         let context = contexts.get(touch.identifier)
         move(touch, context)
     }
@@ -78,7 +74,6 @@ document.addEventListener('touchmove', event => {
 
 
 document.addEventListener('touchend', event => {
-    console.log(event.changedTouches)
     for (let touch of event.changedTouches) {
         console.log(touch.clientX, touch.clientY)
         let context = contexts.get(touch.identifier)
@@ -95,15 +90,21 @@ document.addEventListener('touchcancel', event => {
     }
 })
 
-let handler, startX, startY,
-    isPan = false,
-    isTab = true,
-    isPress = false
+// let handler, startX, startY,
+//     isPan = false,
+//     isTab = true,
+//     isPress = false
 
 let start = (point, context) => {
     //console.log('start', point.clientX, point.clientY)
     context.startX = point.clientX
     context.startY = point.clientY
+
+    context.points = [{
+        t: Date.now(),
+        x: point.clientX,
+        y: point.clientY
+    }]
 
     context.isTab = true
     context.isPan = false
@@ -114,7 +115,7 @@ let start = (point, context) => {
         context.isPan = false
         context.isPress = true
         context.handler = null
-        console.log('pressstart')
+        dispatch('pressstart', context)
     }, 500)
 }
 
@@ -125,39 +126,60 @@ let move = (point, context) => {
         context.isTab = false
         context.isPan = true
         context.isPress = false
-        console.log('panstart')
+        dispatch('panstart', context)
         clearTimeout(context.handler)
     }
 
     if (context.isPan) {
         console.log(dx, dy)
-        console.log('pan')
+        dispatch('pan', context)
     }
 
-    console.log('move', point.clientX, point.clientY)
+    context.points.push({
+        t: Date.now(),
+        x: point.clientX,
+        y: point.clientY
+    })
+
 
 }
 
 let end = (point, context) => {
     if (context.isTab) {
-        console.log('tab')
-        dispatch('tab', {})
+        dispatch('tab', context)
         clearTimeout(context.handler)
     }
     if (context.isPan) {
         // console.log(dx, dy)
-        console.log('panend')
+        dispatch('panend', context)
     }
     if (context.isPress) {
         // console.log(dx, dy)
-        console.log('pressend')
+        dispatch('pressend', context)
     }
-    console.log('end', point.clientX, point.clientY)
+    context.points = context.points.filter(point => Date.now() - point.t < 500)
+
+    let d, v
+    if (!context.points.length) {
+        v = 0
+    } else {
+        d = Math.sqrt((point.clientX - context.points[0].y) ** 2 + (point.clientX - context.points[0].y) ** 2)
+
+        v = d / (Date.now() - context.points[0].t)
+    }
+
+    if (v > 1.5) { // 像素每毫秒 px/ms
+        dispatch('flick', context)
+        context.isFlick = true
+    } else {
+        context.isFlick = false
+    }
+    dispatch('end', context)
 }
 
 let cancel = (point, context) => {
     clearTimeout(context.handler)
-    console.log('cancel', point.clientX, point.clientY)
+    dispatch('cancel', context)
 }
 
 function dispatch(type, properties) {
